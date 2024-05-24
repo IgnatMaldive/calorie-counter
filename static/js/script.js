fc2488dc33a08a03eb95cc75c4b0d6afe9a8a08e
Ignat Maldive <36450446+IgnatMaldive@users.noreply.github.com>
2024-05-24 22:42:42 +0200

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,49 @@
+const upload_button = document.querySelector("#upload");
+const calorie_count = document.querySelector("#calorie-count");
+const food_image = document.querySelector("#food-image");
+const camera_icon = document.querySelector("#camera-icon");
+
+upload_button.addEventListener("click", () => {
+    // ask to upload a file or take an image
+    const file_input = document.createElement("input");
+    file_input.type = "file";
+    file_input.accept = "image/*";
+    file_input.click();
+
+    file_input.addEventListener("change", () => {
+        loading();
+
+        const file = file_input.files[0];
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            food_image.src = e.target.result;
+            camera_icon.style.display = "none";  // Hide camera icon
+        };
+        reader.readAsDataURL(file);
+
+        const fd = new FormData();
+        fd.append("image", file);
+
+        fetch("/upload", {
+            method: "POST",
+            body: fd
+        }).then(response => response.json())
+        .then(data => {
+            stop_loading();
+            calorie_count.textContent = data.calories.total;
+        });
+    });
+});
+
+function loading() {
+    document.querySelector("#upload").style.display = "none";
+    document.querySelector("#calorie-count").style.display = "none";
+    document.querySelector("#spinner").style.display = "block";
+}
+
+function stop_loading() {
+    document.querySelector("#spinner").style.display = "none";
+    document.querySelector("#upload").style.display = "block";
+    document.querySelector("#calorie-count").style.display = "block";
+}
